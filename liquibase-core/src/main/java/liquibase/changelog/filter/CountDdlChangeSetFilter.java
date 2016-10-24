ca660385c8d4efc3cbf50c3de4fd61f22bc09bae
willyzhuo <willy.zhuo@ebaotech.com>
2016-10-24 19:19:55 +0800

    ignore failOnError changeSet in markNextChangeSetRan

--- liquibase-core/src/main/java/liquibase/changelog/filter/CountDdlChangeSetFilter.java
+++ liquibase-core/src/main/java/liquibase/changelog/filter/CountDdlChangeSetFilter.java
@@ -3,17 +3,26 @@ package liquibase.changelog.filter;
 import liquibase.changelog.ChangeSet;
 
 public class CountDdlChangeSetFilter implements ChangeSetFilter {
-	
+
   private boolean found = false;
 
-  public CountDdlChangeSetFilter() {
-  }
+  public CountDdlChangeSetFilter() {}
 
   public ChangeSetFilterResult accepts(ChangeSet changeSet) {
-    if (!found && !changeSet.isAlwaysRun()) {
+    if (!found && match(changeSet)) {
       found = true;
       ChangeSet.setChangeSetMarkedRan(changeSet.getIdentifier());
     }
     return new ChangeSetFilterResult(true, "", this.getClass());
+  }
+
+  private boolean match(ChangeSet changeSet) {
+    if (changeSet.isAlwaysRun()) {
+      return false;
+    }
+    if (changeSet.getFailOnError() != null && changeSet.getFailOnError()) {
+      return false;
+    }
+    return true;
   }
 }
